06d6e25ef1217e63003cd82ef07cfe96dc05b569
ZyeLangX <131007575+zyelangx@users.noreply.github.com>
2024-01-21 09:51:24 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // ex Mongodb Atlas : mongodb+srv://user:password@cluster.xxx (remove <password> with ur password)
+    dbURI: 'mongodb+srv://kimzz:WQpJjtnJ8R9HffPi@cluster0.m56e0mj.mongodb.net/?retryWrites=true&w=majority'
+};
